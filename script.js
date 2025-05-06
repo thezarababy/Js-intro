dee6b54840480d63b439e117d41398bd72424a6c
Okoduwa Joy Akhuemose <thezarababy>
2025-05-06 16:41:06 +0100

    practice one

---
+++ script.js
@@ -0,0 +1,86 @@
+
+let Joy ="today is joy's birthday";
+console.log(Joy)
+const student = {
+    name: "Joy",
+    age:21,
+    isPregnant: true,
+}
+// console.log(student)
+// console.log(typeof Joy)
+// console.log(typeof student)
+// const myArray =["Amaka","James",345,true, {name:"joy",},47,34]
+// console.log(myArray);
+// console.log(typeof myArray)
+// console.log(myArray[4]);
+// console.log("student"=="Joy")
+// //  const myAge = prompt("Enter your age");
+// // alert(myAge);
+
+// let valueOne = parseInt(prompt("enter your first Number"));
+// let valueTwo = parseInt(prompt("enter your second number"));
+// let ans = valueOne + valueTwo;
+// alert(ans);
+//write a program that determines wheter a given number is positive or negative.
+//  let num= -18;
+//  if (num < 0 ){
+// console.log("this a negative number ")
+//  }
+//  else
+ //write a program that checks if the number is even or odd
+// let num = parseInt(prompt("enter a number"));
+// if (num % 2 === 0){
+//     alert("This is an even number");
+
+// }
+// else{
+//     alert("this is an odd number");
+// }
+//conditional statement
+//  if(12>14){
+//  console.log("12 is the highest number")
+// } else{
+//     console.log("12 is not the highest number")
+// }
+
+// const age=parseInt(prompt("enter your age"));
+// alert(age);
+
+// const idNumber = parseInt(prompt("enter your ID number here"));
+// if(idNumber  < 0){
+//     alert(`${idNumber} is a negative number`);
+// } else if(idNumber > 0){
+//     alert(`${idNumber} is a positive number`);
+// }
+
+//Write a program that determines the greater of two numbers
+
+
+//  const greaterNumber= parseInt(prompt("enter the greater number"));
+//  const lesserNumber = parseInt(prompt("enter the lesser number"));
+// if(greaterNumber>lesserNumber){
+//     alert(`${greaterNumber} is the greaterNumber`);
+// }else if(greaterNumber===0){
+//     alert(`${lesserNumber} is the greaterNumber`);
+// }else if(lesserNumber===0){
+//     alert(`${greaterNumber} is the greaterNumber`);
+// }else{
+//    alert(`${greaterNumber} and ${lesserNumber}`);
+// }
+//Write a program that transforms  a numerical grade to a letter grade(e.g grade 0-39 should display "f" grade 40-44 should display"E", GRADE 45-49 should display "D", grade 50-59 should display "C", grade 60-69 should display "B", grade 70-100 should display "A").
+const myGrade = parseInt(prompt("enter your grade"));
+if(myGrade <= 39){
+    alert(`${myGrade} F is a poor result`);
+}else if (myGrade >=40 && myGrade <=44){
+    alert(`${myGrade} E is a bad result`);
+}else if (myGrade >=45 && myGrade <=49){
+    alert(`${myGrade} D is a not so good `);
+}else if (myGrade >=50 && myGrade <=59){
+    alert(`${myGrade} C is a good result you can do better `);
+}else if (myGrade >=60 && myGrade <=69){
+    alert(`${myGrade} B is Excellent `);
+}else if (myGrade >=70 && myGrade <=100){
+    alert(`${myGrade} A is Brillliant Result `);
+}else{
+    alert("invalid")
+}
