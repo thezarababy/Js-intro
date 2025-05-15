142a97f9b903fe257d0708f9d3cd037cb8dd8ff3
Okoduwa Joy Akhuemose <thezarababy>
2025-05-15 16:08:09 +0100

    first version

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-let Joy ="today is joy's birthday";
-console.log(Joy)
-const student = {
-    name: "Joy",
-    age:21,
-    isPregnant: true,
-}
+// let Joy ="today is joy's birthday";
+// console.log(Joy)
+// const student = {
+//     name: "Joy",
+//     age:21,
+//     isPregnant: true,
+// }
 // console.log(student)
 // console.log(typeof Joy)
 // console.log(typeof student)
@@ -68,19 +68,111 @@ const student = {
 //    alert(`${greaterNumber} and ${lesserNumber}`);
 // }
 //Write a program that transforms  a numerical grade to a letter grade(e.g grade 0-39 should display "f" grade 40-44 should display"E", GRADE 45-49 should display "D", grade 50-59 should display "C", grade 60-69 should display "B", grade 70-100 should display "A").
-const myGrade = parseInt(prompt("enter your grade"));
-if(myGrade <= 39){
-    alert(`${myGrade} F is a poor result`);
-}else if (myGrade >=40 && myGrade <=44){
-    alert(`${myGrade} E is a bad result`);
-}else if (myGrade >=45 && myGrade <=49){
-    alert(`${myGrade} D is a not so good `);
-}else if (myGrade >=50 && myGrade <=59){
-    alert(`${myGrade} C is a good result you can do better `);
-}else if (myGrade >=60 && myGrade <=69){
-    alert(`${myGrade} B is Excellent `);
-}else if (myGrade >=70 && myGrade <=100){
-    alert(`${myGrade} A is Brillliant Result `);
-}else{
-    alert("invalid")
-}
+// const myGrade = parseInt(prompt("enter your grade"));
+// if(myGrade <= 39){
+//     alert(`${myGrade} F is a poor result`);
+// }else if (myGrade >=40 && myGrade <=44){
+//     alert(`${myGrade} E is a bad result`);
+// }else if (myGrade >=45 && myGrade <=49){
+//     alert(`${myGrade} D is a not so good `);
+// }else if (myGrade >=50 && myGrade <=59){
+//     alert(`${myGrade} C is a good result you can do better `);
+// }else if (myGrade >=60 && myGrade <=69){
+//     alert(`${myGrade} B is Excellent `);
+// }else if (myGrade >=70 && myGrade <=100){
+//     alert(`${myGrade} A is Brillliant Result `);
+// }else{
+//     alert("invalid")
+// }
+//write a program that calculates the BMI and categorizes it. The formula for BMI : weight / (height*height).
+//UNderweight (below 18.5), Healthy weight (18.5 to 24.9), Overweight (25 to 29.9 )and obese(30 0r greater)
+
+// let W= 12;
+// let H = 63;
+
+//methods and properties
+//Array
+
+// function callMyName(){
+//     var name = "ife";
+//     for(Let i = 0 )
+// }
+
+// let myStr ="Hello , Javascript World";
+// console.log(myStr);
+// console.log(myStr.toUpperCase());
+
+// function multiplyNum(a,b){
+//     console.log(a * b)
+// }
+// multiplyNum(3,5)
+
+// function greet(name, gender){
+//     console.log("Hello world", name,gender);
+// }
+
+//    greet("joy", "female");
+//    greet("ife", "male");
+
+// //function expression
+//    function sumOf2Numbers (num1,num2){
+   
+//     let result = num1 + num2;
+//     console.log(result)
+
+//    }
+//    //function declaration
+//    function sumOf3Numbers (num1,num2, num3){
+   
+//     let result = num1 + num2 + num3;
+//     return result;
+
+//    }
+//    sumOf2Numbers(4,7)
+//    sumOf3Numbers(4,7,5)
+// function number(){
+//     prompt("enter the number");
+    
+// }
+//    number();
+
+//    function Subtraction(num1 ,num2){
+//     //let answer = num1 - num2;
+//     console.log(num1-num2)
+//    }
+//    Subtraction(6,4);
+//    Subtraction(11,5)
+//create a function that takes any number from a user and checks if that number is greater than 6
+  // function input (){
+
+ //  let num = prompt("enter a number");
+   // if(num > 6){
+      //  alert("correct");
+   // }else if(num < 6 ){
+        //alert("wrong");
+    //}else{
+        //alert("invalid")
+  //  }
+    
+  // }
+   
+//input()
+
+// function userName(){
+//     let myName = prompt("enter your name");
+//    console.log(myName.length);
+// }
+// userName()
+//create a function that takes 4 numbers and returns the highest number
+
+//  that takes a number and checks if the number is even or odd
+
+function evenOddNumber(){
+    const num = prompt("enter a number");
+    if (num % 2==0){
+        console.log(`${num} is an even number`);
+    }else{
+        console.log(`${num} is an odd number`)
+    }
+};
+evenOddNumber()
